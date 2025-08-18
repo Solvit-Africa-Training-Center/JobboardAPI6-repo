65237550902412edeedbb5d2f2ea8cbc8736293d
Thiedra <mkthierry14@gmail.com>
2025-08-18 15:04:26 +0200

    fixed models that is running

--- Jobs/models.py
+++ Jobs/models.py
@@ -32,7 +32,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     last_name= models.CharField(max_length=50)
     role=models.CharField(max_length=40, choices= Role.choices)
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
-    is_active= models.BooleanField(default=False)
+    is_active= models.BooleanField(default=True)
     is_superuser= models.BooleanField(default=False)
     is_staff= models.BooleanField(default=True)
 
