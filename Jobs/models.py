21191c88d6e546de210af1576ea73cfe47eb2909
Thiedra <mkthierry14@gmail.com>
2025-08-18 11:22:05 +0200

    Fixed password argument typo in custom user model

--- Jobs/models.py
+++ Jobs/models.py
@@ -7,18 +7,18 @@ class CustomuserManager(BaseUserManager):
     def create_user(self, email, password=None, **kwargs):
         if not email:
             raise ValueError('User must have email address')
-        email= self.normalize_email[email]
+        email= self.normalize_email(email)
         user= self.model(email=email, **kwargs)
         user.set_password(password)
         user.save()
         return user
     
-    def create_superuser(self, email, passowrd=None, **kwargs):
+    def create_superuser(self, email, password=None, **kwargs):
         kwargs.setdefault('is_staff', True)
         kwargs.setdefault('is_superuser', True)
         kwargs.setdefault('role', User.Role.ADMIN)
 
-        return self.create_user(email,passowrd, **kwargs)
+        return self.create_user(email,password, **kwargs)
     
 class User(AbstractBaseUser, PermissionsMixin):
     class Role(models.TextChoices):
